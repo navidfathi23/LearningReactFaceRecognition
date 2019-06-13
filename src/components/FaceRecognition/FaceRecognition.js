7b18919699ba2c8b2bff6ab9bf337abb69947cc8
Navid <navidfathi98@gmail.com>
2019-06-14 00:06:39 +0430

    Updated till Face Detection Box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const FaceRecognition = ({imageUrl}) =>{
+const FaceRecognition = ({imageUrl , box}) =>{
 	return(
 		<div className="center ma">
 			<div className="absolute mt2 mb4">
-				<img src={imageUrl} alt="" className="mt4 br4" width='500px' height='auto'/>
+					<img id='inputimage' src={imageUrl} alt="" className="mt4 br4" width='700px' height='auto'/>
+				<div style={{position: 'absolute', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', cursor: 'pointer', boxShadow : '0 0 0 3px #149df2 inset' ,
+					top: box.topRow , right : box.rightCol , left : box.leftCol , bottom: box.bottomRow}}>
+				</div>	
 			</div>	
 		</div>
 	);
 }
 
 export default FaceRecognition;
-
-// https://cdn.images.express.co.uk/img/dynamic/11/590x/skincare-994444.jpg?r=1532587813853
